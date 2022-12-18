23dc49b7780c563c6e3f4438eee63a1bbc37f95e
Balint Balazs <32197316+balintbalazs@users.noreply.github.com>
2022-12-18 15:20:55 +0100

    day17 part2 - too slow

--- src/bin/day17.rs
+++ src/bin/day17.rs
@@ -16,10 +16,11 @@ fn rocks() -> Vec<Vec<String>> {
     ]
 }
 
-fn intersects(part_of_well: &[Vec<char>], rock: &Vec<String>, left: usize) -> bool {
+fn intersects(well: &Vec<Vec<char>>, rock: &Vec<String>, left: usize, bottom: usize) -> bool {
     for row in 0..rock.len() {
         for (col, char) in rock[row].chars().enumerate() {
-            if char == '#' && part_of_well[row][col + left] == '#' {
+            let row = (row + bottom) % well.len();
+            if char == '#' && well[row][col + left] == '#' {
                 return true;
             }
         }
@@ -27,32 +28,39 @@ fn intersects(part_of_well: &[Vec<char>], rock: &Vec<String>, left: usize) -> bo
     false
 }
 
-fn part1(jet_pattern: &str, num_rocks: usize) -> usize {
+fn drop_rocks(jet_pattern: &str, num_rocks: usize) -> usize {
     let mut total_height = 0;
     let rocks = rocks();
     const WIDTH: usize = 7;
+    const HEIGHT: usize = 1_000;
     // worst case - 2022 vertical bars stacked on top of each other
     let mut i = 0;
-    let mut well = vec![vec!['.'; WIDTH]; 2022 * 4 + 10];
+    let mut well = vec![vec!['.'; WIDTH]; HEIGHT];
     for r in 0..num_rocks {
+        if r % 5 == 0 && i == 0 {
+            println!("All repeats after {i} ticks, and rock {r}");
+        }
         // spawn
         let rock = &rocks[r % rocks.len()];
         let mut bottom = total_height + 3;
         let mut left = 2;
+        for row in total_height..bottom+rock.len() {
+            well[row % HEIGHT] = vec!['.'; WIDTH];
+        }
 
         loop {
             // push
             match &jet_pattern[i..=i] {
                 ">" => {
                     if left + rock[0].len() < WIDTH {
-                        if !intersects(&well[bottom..bottom + rock.len()], rock, left + 1) {
+                        if !intersects(&well, rock, left + 1, bottom) {
                             left += 1;
                         }
                     }
                 }
                 "<" => {
                     if left > 0 {
-                        if !intersects(&well[bottom..bottom + rock.len()], rock, left - 1) {
+                        if !intersects(&well, rock, left - 1, bottom) {
                             left -= 1;
                         }
                     }
@@ -62,7 +70,7 @@ fn part1(jet_pattern: &str, num_rocks: usize) -> usize {
             i += 1;
             i %= jet_pattern.len();
             // fall
-            if bottom > 0 && !intersects(&well[bottom - 1..bottom - 1 + rock.len()], rock, left) {
+            if bottom > 0 && !intersects(&well, rock, left, bottom - 1) {
                 bottom -= 1;
             } else {
                 // settle
@@ -70,7 +78,7 @@ fn part1(jet_pattern: &str, num_rocks: usize) -> usize {
                     for (col, char) in rock[row].chars().enumerate() {
                         if char == '#' {
                             // don't overwrite other rocks with air
-                            well[bottom + row][left + col] = char;
+                            well[(bottom + row) % HEIGHT][left + col] = char;
                         }
                     }
                 }
@@ -93,7 +101,7 @@ fn part1(jet_pattern: &str, num_rocks: usize) -> usize {
 fn main() {
     // Read the input from the file
     let input = fs::read_to_string("inputs/day17.txt").expect("Failed to read file");
-    dbg!(part1(&input, 2022));
+    dbg!(drop_rocks(&input, 2022));
 }
 
 #[cfg(test)]
@@ -105,6 +113,11 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(3068, part1(TEST_DATA, 2022));
+        assert_eq!(3068, drop_rocks(TEST_DATA, 2022));
+    }
+
+    #[test]
+    fn test_part2() {
+        dbg!(drop_rocks(TEST_DATA, 1_000_000_000));
     }
 }
