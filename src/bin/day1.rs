1192c47b9431481da464aa9987ba79e4e1ebcdb7
Balint Balazs <balint.balazs@bruker.com>
2023-12-01 15:02:14 +0100

    prepare for 2023

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -3,15 +3,5 @@ use std::fs;
 fn main() {
     // Read the input from the file
     let input = fs::read_to_string("inputs/day1.txt").expect("Failed to read file");
-    let mut elves: Vec<u32> = input
-        .split("\r\n\r\n")
-        .map(|elf| {
-            elf.lines()
-                .map(|line| line.parse::<u32>().unwrap())
-                .sum::<u32>()
-        })
-        .collect();
-    elves.sort();
-    let top3: u32 = elves.iter().rev().take(3).sum();
-    dbg!(top3);
+    println!("Hello day1")
 }
