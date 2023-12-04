396fb89b103abea0933a15ea52b5725cdabc1178
Balint Balazs <32197316+balintbalazs@users.noreply.github.com>
2023-12-04 21:46:58 +0100

    day4 part1

--- src/bin/day4.rs
+++ src/bin/day4.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{collections::HashSet, fs};
 
 fn main() {
     // Read the input from the file
@@ -7,10 +7,26 @@ fn main() {
 
     let part1 = lines.map(|line| {
         let (_, numbers) = line.split_once(':').unwrap();
-        let (winning, ticket) = numbers.split_once('|').unwrap();
-        
-        todo!()
-    });
+        let (winning_numbers, numbers) = numbers.split_once('|').unwrap();
 
-    todo!()
+        let winning_numbers = winning_numbers
+            .trim()
+            .split(' ')
+            .filter(|n| !n.is_empty())
+            .map(|n| n.parse::<u32>().unwrap())
+            .collect::<HashSet<_>>();
+
+        let count = numbers
+            .trim()
+            .split(' ')
+            .filter(|n| !n.is_empty())
+            .map(|n| n.parse::<u32>().unwrap())
+            .filter(|num| winning_numbers.get(num).is_some())
+            .count();
+
+        let score = if count > 0 { 1usize << (count - 1)} else { 0 };
+        score
+    }).sum::<usize>();
+
+    dbg!(part1);
 }
