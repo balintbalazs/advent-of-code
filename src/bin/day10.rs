7fba80e29673b3abbc34ce69ba19d5dc871e2fbb
Balint Balazs <32197316+balintbalazs@users.noreply.github.com>
2023-12-10 19:24:47 +0100

    day10 part 2

--- src/bin/day10.rs
+++ src/bin/day10.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{collections::VecDeque, fs};
 
 #[derive(Clone, Copy, Debug)]
 enum Direction {
@@ -59,6 +59,8 @@ fn main() {
         dirs.push(Down);
     }
 
+    let starting_dirs = [dirs[0], dirs[1]];
+
     assert_eq!(starts.len(), 2);
 
     let mut part1 = 1;
@@ -103,4 +105,90 @@ fn main() {
     }
 
     dbg!(part1);
+
+    // replace starting tile with matching pipe
+    tiles[start.0][start.1] = match starting_dirs {
+        [Up, Down] | [Down, Up] => '|',
+        [Left, Right] | [Right, Left] => '-',
+        [Left, Up] | [Up, Left] => 'J',
+        [Right, Up] | [Up, Right] => 'L',
+        [Left, Down] | [Down, Left] => '7',
+        [Right, Down] | [Down, Right] => 'F',
+        _ => panic!("invalid starting directions"),
+    };
+
+    // create bigger maze, replace every tile by 3x3 sub-tiles
+    // '.' is for empty space
+    // 'x' is for wall
+    let width = tiles[0].len();
+    let height = tiles.len();
+
+    let mut maze = vec![vec![' '; width * 3]; height * 3];
+
+    for r in 0..height {
+        for c in 0..width {
+            let template = match tiles[r][c] {
+                'F' => [['.', '.', '.'], ['.', 'x', 'x'], ['.', 'x', '.']],
+                'J' => [['.', 'x', '.'], ['x', 'x', '.'], ['.', '.', '.']],
+                '7' => [['.', '.', '.'], ['x', 'x', '.'], ['.', 'x', '.']],
+                'L' => [['.', 'x', '.'], ['.', 'x', 'x'], ['.', '.', '.']],
+                '|' => [['.', 'x', '.'], ['.', 'x', '.'], ['.', 'x', '.']],
+                '-' => [['.', '.', '.'], ['x', 'x', 'x'], ['.', '.', '.']],
+                '.' => [['.', '.', '.'], ['.', '.', '.'], ['.', '.', '.']],
+                _ => panic!("invalid tile"),
+            };
+            for rr in 0..3 {
+                for cc in 0..3 {
+                    maze[3 * r + rr][3 * c + cc] = template[rr][cc];
+                }
+            }
+        }
+    }
+
+    // now in upscaled maze to flood fill from (0,0)
+    // label outside tiles with 'o'
+    // assume (0,0) is outside
+    let mut q = VecDeque::new();
+    q.push_back((0, 0));
+    while let Some((r, c)) = q.pop_front() {
+        if maze[r][c] == '.' {
+            maze[r][c] = 'o'; //outside
+            if r > 0 {
+                q.push_back((r - 1, c));
+            }
+            if c > 0 {
+                q.push_back((r, c - 1));
+            }
+            if r < height * 3 - 1 {
+                q.push_back((r + 1, c));
+            }
+            if c < width * 3 - 1 {
+                q.push_back((r, c + 1));
+            }
+        }
+    }
+
+    // count tiles on the inside
+    // this assumes there is only a single loop
+    // tile is on the inside if all 9 sub-tiles are empty '.' or walls 'x'
+    let mut part2 = 0;
+
+    for r in 0..height {
+        'tiles: for c in 0..width {
+            let mut count = 0;
+            for rr in 0..3 {
+                for cc in 0..3 {
+                    let m = maze[3 * r + rr][3 * c + cc];
+                    if m == '.' || m == 'x' {
+                        count += 1;
+                    }
+                }
+            }
+            if count == 9 {
+                part2 += 1;
+            }
+        }
+    }
+
+    dbg!(part2);
 }
