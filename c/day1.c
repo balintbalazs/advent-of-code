f08881781f45f5092997561334a69aba7e564f6d
Balint Balazs <32197316+balintbalazs@users.noreply.github.com>
2024-12-02 20:17:06 +0100

    day1 part 1 in C

--- c/day1.c
+++ c/day1.c
@@ -3,9 +3,6 @@
 
 int main()
 {
-   // printf() displays the string inside quotation
-   printf("Hello, World!\n");
-
    FILE *fptr;
 
    // Open a file in read mode
@@ -28,7 +25,7 @@ int main()
    // Read the content and print it
    while (fscanf(fptr, "%d %d", &left, &right) == 2)
    {
-      printf("left: %d, right: %d\n", left, right);
+      // printf("left: %d, right: %d\n", left, right);
       push_vec(&vec_left, left);
       push_vec(&vec_right, right);
    }
@@ -36,11 +33,19 @@ int main()
    // Close the file
    fclose(fptr);
 
+
+   sort_vec(&vec_left);
+   sort_vec(&vec_right);
+
+   int sum = 0;
    // Print vec_left
    for (size_t i = 0; i < (vec_left.size); i++)
    {
-      printf("left: %d\n", vec_left.head[i]);
+      vec_left.head[i] = abs((vec_left.head[i] - vec_right.head[i]));
+      sum += vec_left.head[i];
    }
+
+   printf("%d\n", sum);
 
    free_vec(vec_left);
    free_vec(vec_right);
--- c/vec.h
+++ c/vec.h
@@ -1,14 +1,87 @@
 #ifndef VEC_H
 #define VEC_H
 
-typedef struct{
-    int* head;
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+    int *head;
     int capacity;
     int size;
 } Vec;
 
-Vec create_vec(int capacity);
-int push_vec(Vec* vector, int element);
-void free_vec(Vec vector);
+#define OK 0
+#define ERR 1
+
+Vec create_vec(int capacity)
+{
+    int *head = malloc(capacity * sizeof(int));
+
+    Vec vec;
+    vec.head = head;
+    vec.capacity = capacity;
+    vec.size = 0;
+
+    return vec;
+}
+
+int double_vec_capacity(Vec *vector)
+{
+    vector->capacity *= 2;
+    vector->head = realloc(vector->head, vector->capacity * sizeof(int));
+
+    if (vector->head == NULL)
+    {
+        perror("Couldn't double vector capacity\n");
+        return ERR;
+    }
+
+    return OK;
+}
+
+int push_vec(Vec *vector, int element)
+{
+    if (vector->size == vector->capacity)
+    {
+        int ret = double_vec_capacity(vector);
+        if (ret == ERR)
+        {
+            perror("Couldn't push to vector\n");
+            return ERR;
+        }
+    }
+    vector->head[vector->size] = element;
+    vector->size += 1;
+    return OK;
+}
+
+void free_vec(Vec vector)
+{
+    free(vector.head);
+}
+
+// from https://en.cppreference.com/w/c/algorithm/qsort
+int compare_ints(const void *a, const void *b)
+{
+    int arg1 = *(const int *)a;
+    int arg2 = *(const int *)b;
+
+    if (arg1 < arg2)
+        return -1;
+    if (arg1 > arg2)
+        return 1;
+    return 0;
+
+    // return (arg1 > arg2) - (arg1 < arg2); // possible shortcut
+
+    // return arg1 - arg2; // erroneous shortcut: undefined behavior in case of
+    // integer overflow, such as with INT_MIN here
+}
+
+void sort_vec(Vec *vector)
+{
+    qsort(vector->head, vector->size, sizeof(int), compare_ints);
+}
 
 #endif
